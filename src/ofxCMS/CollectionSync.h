2f657afc4475a44c7dbd1d554dbbe48f69b0e249
Mark van de Korput <mark.korput@gmail.com>
2017-05-17 10:47:35 +0200

    CollectionSync works on ObjectCollection instead of ModelCollection

--- src/ofxCMS/CollectionSync.h
+++ src/ofxCMS/CollectionSync.h
@@ -1,29 +1,29 @@
 #pragma once
 
-#include "ModelCollection.h"
+#include "ObjectCollection.h"
 
 namespace ofxCMS {
     template<class ModelClass>
-    class CollectionSync : public ModelCollection<ModelClass> {
+    class CollectionSync : public ObjectCollection<ModelClass> {
         public: // methods
 
             CollectionSync() : target(NULL), source(nullptr){}
             ~CollectionSync(){ destroy(); }
 
-            void setup(ModelCollection<ModelClass> *target, shared_ptr<ModelCollection<ModelClass>> source);
+            void setup(ObjectCollection<ModelClass> *target, shared_ptr<ObjectCollection<ModelClass>> source);
             void destroy();
 
-            shared_ptr<ModelCollection<ModelClass>> getSource() const { return source; }
+            shared_ptr<ObjectCollection<ModelClass>> getSource() const { return source; }
 
         private: // attributes
 
-            ModelCollection<ModelClass> *target;
-            shared_ptr<ModelCollection<ModelClass>> source;
+            ObjectCollection<ModelClass> *target;
+            shared_ptr<ObjectCollection<ModelClass>> source;
     };
 }
 
 template<class ModelClass>
-void ofxCMS::CollectionSync<ModelClass>::setup(ModelCollection<ModelClass> *target, shared_ptr<ModelCollection<ModelClass>> source){
+void ofxCMS::CollectionSync<ModelClass>::setup(ObjectCollection<ModelClass> *target, shared_ptr<ObjectCollection<ModelClass>> source){
     destroy();
 
     this->target = target;
@@ -43,15 +43,15 @@ void ofxCMS::CollectionSync<ModelClass>::setup(ModelCollection<ModelClass> *targ
 
     // actively monitor for new models added to source
     source->addEvent.addListener([&](ModelClass& model){
-        if(!this->target->has(model.cid())){
-            this->target->add(this->source->findByCid(model.cid())); // need to convert ref var to shared_ptr
+        if(!this->target->has(&model)){
+            this->target->add(this->source->find(&model)); // need to convert ref var to shared_ptr
         }
     }, this);
 
     // actively montor for models removed from source
     source->removeEvent.addListener([this](ModelClass& model){
-        if(this->target->has(model.cid()))
-            this->target->removeByCid(model.cid());
+        if(this->target->has(&model))
+            this->target->remove(&model);
     }, this);
 }
 
